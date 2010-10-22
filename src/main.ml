45eea81cb44e5780b23b806635e1660fa952a49b
Ben Kirzhner <bkirz@cs.umd.edu>
2010-10-22 19:46:34 -0400

    Usage displays instead of an exception.

--- src/main.ml
+++ src/main.ml
@@ -5,9 +5,9 @@
 *)
 
 let () =
-  let filename = Sys.argv.(1) in 
   if Array.length Sys.argv < 2 then
-    Printf.printf "usage: %s file.rb\n" filename 
+    Printf.printf "usage: %s file.rb\n" Sys.argv.(0)
   else
+    let filename = Sys.argv.(1) in 
     let ast = Parse_helper.parse_ruby_file filename in
     Ast_printer.print_ast ast
