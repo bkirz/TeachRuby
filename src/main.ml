bdbcd2a310a4b7d5d7c3f03fc73f4b38541c968f
Ben Kirzhner <bkirz@cs.umd.edu>
2010-11-09 17:32:48 -0500

    Transforms AST into CFG before unparsing.

--- src/main.ml
+++ src/main.ml
@@ -1,8 +1,11 @@
 (*
   Test driver for TeachRuby.
     1. Parses a ruby file into an AST.
-    2. Unparses the AST into ruby code and prints it.
+    2. Convert the AST into a RIL CFG.
+    3. Unparses the CFG into ruby code and prints it.
 *)
+
+open Cfg_printer
 
 let () =
   if Array.length Sys.argv < 2 then
@@ -10,4 +13,6 @@ let () =
   else
     let filename = Sys.argv.(1) in 
     let ast = Parse_helper.parse_ruby_file filename in
-    Ast_printer.print_ast ast
+    let cfg = Cfg_refactor.refactor_ast ast in
+    let program_text = CodePrinter.string_of_cfg cfg in
+      Printf.printf "%s\n" program_text
