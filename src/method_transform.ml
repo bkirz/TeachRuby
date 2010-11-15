2d68e4e07a042cf136c8d4ef2f037822a2486bb6
greg <greg@birdMachine.(none)>
2010-11-15 17:56:42 -0500

    Imperative method invocation via ruby method wrapper

--- src/method_transform.ml
+++ src/method_transform.ml
@@ -1,7 +1,24 @@
 open Ast
 
+let get_meth_atom e = match e with
+	| E_Identifier(ID_Lowercase, s, p) -> E_Literal(Lit_Atom([StrChars(s)]), Lexing.dummy_pos)
+	| _ -> failwith "get_meth_atom error"
+
 let change_meth_call e = match e with
-	| E_MethodCall(e1, e_list, e_opt, p) -> E_Empty
+	| E_MethodCall(e1, e_list, e_opt, p) ->
+		E_MethodCall(
+			E_Identifier(ID_Lowercase, "__p", Lexing.dummy_pos),
+			(get_meth_atom e1)::e_list,
+			e_opt,
+			p)
 	| _ -> e
 
-let do_meth_change ast = Ast.mod_ast change_meth_call ast
+let do_meth_change ast =
+	let prog = Ast.mod_ast change_meth_call ast in
+	(E_MethodCall(
+		E_Identifier(ID_Lowercase, "require", Lexing.dummy_pos),
+		[E_Literal(
+			Lit_String(String_Single("ruby/teach_ruby.rb")),
+			Lexing.dummy_pos)],
+		None,
+		Lexing.dummy_pos))::prog
--- src/test/test.rb
+++
@@ -1,7 +0,0 @@
-if true then
-	puts "Hello, world!"
-end
-
-if true: puts "Hello, world!" end
-
-puts "Hello, world!" if true
