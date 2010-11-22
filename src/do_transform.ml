5e544545ca42375d7a278cb3560ff59b6f72c58f
greg <greg@birdMachine.(none)>
2010-11-22 17:25:41 -0500

    banned eval

--- src/do_transform.ml
+++ src/do_transform.ml
@@ -1,7 +1,8 @@
 open Ast
 
 let get_meth_atom e = match e with
-	| E_Identifier(ID_Lowercase, s, p) -> E_Literal(Lit_Atom([StrChars(s)]), Lexing.dummy_pos)
+	| E_Identifier(ID_Lowercase, s, p) -> 
+		if (s = "eval") then (failwith "Thou shalt not eval!") else (E_Literal(Lit_Atom([StrChars(s)]), Lexing.dummy_pos))
 	| _ -> failwith "get_meth_atom error"
 
 let change_meth_calls e = match e with
